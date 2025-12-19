bff01a8595775dcdd04d3d30d79bcbf7cb95da4b
YousefBrro <ybrro2003@gmail.com>
2025-12-19 17:03:41 +0300

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,50 @@
-let searchInput = document.getElementById("searchInput")
-let categoryFilter = document.getElementById("categoryFilter")
-let priceSort = document.getElementById("priceSort")
+let products = JSON.parse(localStorage.getItem("products")) || []
+let productsForm = document.querySelector("#productForm")
+let productTable = document.querySelector("#productTable tbody")
 
-searchInput.addEventListener("input", updateDisplay)
-categoryFilter.addEventListener("change", updateDisplay)
-priceSort.addEventListener("change", updateDisplay)
+function addProduct(){
+    productTable.innerHTML = ""
+    products.forEach((product,i) => {
+        productTable.innerHTML +=`
+        <tr>
+            <th>${product.name}</th>
+            <th>${product.price}</th>
+            <th>${product.category}</th>
+            <th><img src = "${product.image}"</th>
+        <td>
+            <button onclick"deleteProduct(${i})">Delete</button>
+            <button onclick"editProduct(${i})">Edit</button>
+        </td>
+</tr>`
+    });
+}
 
+function deleteProduct(i){
+    products.splice(i,1)
+    localStorage.setItem("products" ,JSON.stringify(products))
+    addProduct()
+}
+
+function editProduct(i){
+    let product = products[i]
+    document.getElementById("name").value = product.name
+    document.getElementById("price").value = product.price
+    document.getElementById("category").value = product.category
+    document.getElementById("image").value = product.image
+    deleteProduct(i)
+}
+
+productsForm.addEventListener("submit" , (event) => {
+    event.preventDefault()
+    let newProduct = {
+        name :  document.getElementById("name").value,
+        price : document.getElementById("price").value,
+        category : document.getElementById("category").value,
+        image : document.getElementById("image").value
+    }
+    products.push(newProduct)
+    localStorage.setItem("products",JSON.stringify(products))
+    addProduct()
+    productsForm.reset()
+})
+addProduct()
